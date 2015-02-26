ede0a45ea5d9447f6e58341cf93e253a830f7ddb
sairamdikondawar <sairamdikondawar@gmail.com>
2015-02-26 18:08:42 +0530

    updated beans.xml and wsContext.xml

--- iclub-model/src/main/java/za/co/iclub/pss/ws/model/common/ResponseModel.java
+++ iclub-model/src/main/java/za/co/iclub/pss/ws/model/common/ResponseModel.java
@@ -1,5 +1,7 @@
 package za.co.iclub.pss.ws.model.common;
 
+import java.io.File;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "ResponseModel")
@@ -21,6 +23,23 @@ public class ResponseModel {
 
 	public void setStatusDesc(String statusDesc) {
 		this.statusDesc = statusDesc;
+	}
+	
+	public static void main(String[] args) {
+		File folder = new File("/noformat/github/iclub/26-Feb-2015/insure-club/iclub-ws/src/main/java/za/co/iclub/pss/ws/service");
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles) {
+		    if (file.isFile()) {
+		        
+		    	System.out.println("<bean id=\""+file.getName().replace(".java", "")+"\" class=\"za.co.iclub.pss.ws.service."+file.getName().replace(".java", "")+"\"+>");
+		    	System.out.println("<property name=\""+(Character.toLowerCase(file.getName().replace("Service.java", "DAO").charAt(0)) + file.getName().replace("Service.java", "DAO").substring(1))+"\" ref=\""+(Character.toUpperCase(file.getName().replace("Service.java", "DAO").charAt(0)) + file.getName().replace("Service.java", "DAO").substring(1))+"\" />");
+		    	System.out.println("<property name=\"iclubCommonDAO\" ref=\"IclubCommonDAO\" />");
+		    	System.out.println("</bean>");
+		    	
+		    
+		    }
+		}
 	}
 
 }
