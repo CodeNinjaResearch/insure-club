cddd9e299f784cdde90e5609388c5e0622ca1b7e
sairamdikondawar <sairamdikondawar@gmail.com>
2015-02-27 14:20:58 +0530

    Added labels.properties and intial controller

--- iclub-web/src/main/java/za/co/iclub/pss/web/controller/IclubAccessTypeController.java
+++ iclub-web/src/main/java/za/co/iclub/pss/web/controller/IclubAccessTypeController.java
@@ -1,6 +1,193 @@
 package za.co.iclub.pss.web.controller;
 
-public class IclubAccessTypeController {
-	
- 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.ResourceBundle;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.log4j.Logger;
+import za.co.iclub.pss.web.bean.IclubAccessTypeBean;
+import za.co.iclub.pss.web.util.IclubWebHelper;
+import za.co.iclub.pss.ws.model.IclubAccessTypeModel;
+import za.co.iclub.pss.ws.model.common.ResponseModel;
+
+public class IclubAccessTypeController implements Serializable {
+
+	private static final long serialVersionUID = 6271776777151313314L;
+	private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("iclub-web");
+	private static final Logger LOGGER = Logger.getLogger(IclubAccessTypeController.class);
+	private static final String BASE_URL = "http://" + BUNDLE.getString("ws.host") + ":" + BUNDLE.getString("ws.port") + "/iclub-ws/iclub/IclubAccessTypeService/";
+	private List<IclubAccessTypeBean> beans;
+	private IclubAccessTypeBean bean;
+	private boolean showAddPanel;
+	private boolean showModPanel;
+	private ResourceBundle labelBundle;
+
+	public void addIclubAccessType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: addIclubAccessType");
+		try {
+			if (validateForm(true)) {
+				WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "add");
+				IclubAccessTypeModel model = new IclubAccessTypeModel();
+
+				model.setAtLongDesc(bean.getAtLongDesc());
+				model.setAtShortDesc(bean.getAtShortDesc());
+				model.setAtStatus(bean.getAtStatus());
+
+				ResponseModel response = client.accept(MediaType.APPLICATION_JSON).post(model, ResponseModel.class);
+				client.close();
+				if (response.getStatusCode() == 0) {
+					IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("add.success"), FacesMessage.SEVERITY_INFO);
+					clearForm();
+				} else {
+					IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("add.error") + " :: " + response.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("add.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+
+	public void modIclubAccessType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: modIclubAccessType");
+		try {
+			if (validateForm(false)) {
+				WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "mod");
+				IclubAccessTypeModel model = new IclubAccessTypeModel();
+				model.setAtId(bean.getAtId());
+				model.setAtLongDesc(bean.getAtLongDesc());
+				model.setAtShortDesc(bean.getAtShortDesc());
+				model.setAtStatus(bean.getAtStatus());
+
+				ResponseModel response = client.accept(MediaType.APPLICATION_JSON).put(model, ResponseModel.class);
+				client.close();
+				if (response.getStatusCode() == 0) {
+					IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("mod.success"), FacesMessage.SEVERITY_INFO);
+					clearForm();
+				} else {
+					IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("mod.error") + " :: " + response.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("mod.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+
+	public void delIclubAccessType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: delIclubAccessType");
+		try {
+			WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "del/" + bean.getAtId());
+			Response response = client.accept(MediaType.APPLICATION_JSON).get();
+			if (response.getStatus() == 200) {
+				IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("del.success"), FacesMessage.SEVERITY_INFO);
+				clearForm();
+			} else {
+				IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("del.service.error"), FacesMessage.SEVERITY_ERROR);
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("appltype") + " " + getLabelBundle().getString("del.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+
+	public void clearForm() {
+		showAddPanel = false;
+		showModPanel = false;
+		bean = new IclubAccessTypeBean();
+	}
+
+	public void showAddPanel() {
+		showAddPanel = true;
+		showModPanel = false;
+		bean = new IclubAccessTypeBean();
+	}
+
+	public void showModPanel() {
+		showAddPanel = false;
+		showModPanel = true;
+	}
+
+	public boolean validateForm(boolean flag) {
+		boolean ret = true;
+
+		if (bean.getAtShortDesc() != null && !bean.getAtShortDesc().trim().equalsIgnoreCase("")) {
+			WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "validate/sd/" + bean.getAtShortDesc().trim() + "/" + ((bean.getAtId() == null) ? -999l : bean.getAtId()));
+			ResponseModel message = client.accept(MediaType.APPLICATION_JSON).get(ResponseModel.class);
+			client.close();
+			if (message.getStatusCode() != 0) {
+				IclubWebHelper.addMessage(message.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				ret = ret && false;
+			}
+		}
+
+		if (bean.getAtStatus().equalsIgnoreCase("-1")) {
+			IclubWebHelper.addMessage(getLabelBundle().getString("val.select.valid"), FacesMessage.SEVERITY_ERROR);
+			ret = ret && false;
+		}
+
+		return ret;
+	}
+
+	public List<IclubAccessTypeBean> getBeans() {
+		WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "list");
+		Collection<? extends IclubAccessTypeModel> models = new ArrayList<IclubAccessTypeModel>(client.accept(MediaType.APPLICATION_JSON).getCollection(IclubAccessTypeModel.class));
+		client.close();
+		beans = new ArrayList<IclubAccessTypeBean>();
+		for (IclubAccessTypeModel model : models) {
+			IclubAccessTypeBean bean = new IclubAccessTypeBean();
+			bean.setAtId(model.getAtId());
+			bean.setAtLongDesc(model.getAtLongDesc());
+			bean.setAtShortDesc(model.getAtShortDesc());
+			bean.setAtStatus(model.getAtStatus());
+			beans.add(bean);
+		}
+		return beans;
+	}
+
+	public void setBeans(List<IclubAccessTypeBean> beans) {
+		this.beans = beans;
+	}
+
+	public IclubAccessTypeBean getBean() {
+		if (bean == null)
+			bean = new IclubAccessTypeBean();
+		return bean;
+	}
+
+	public void setBean(IclubAccessTypeBean bean) {
+		this.bean = bean;
+	}
+
+	public boolean isShowAddPanel() {
+		return showAddPanel;
+	}
+
+	public void setShowAddPanel(boolean showAddPanel) {
+		this.showAddPanel = showAddPanel;
+	}
+
+	public boolean isShowModPanel() {
+		return showModPanel;
+	}
+
+	public void setShowModPanel(boolean showModPanel) {
+		this.showModPanel = showModPanel;
+	}
+
+	public ResourceBundle getLabelBundle() {
+
+		labelBundle = FacesContext.getCurrentInstance().getApplication().getResourceBundle(FacesContext.getCurrentInstance(), "labels");
+		return labelBundle;
+	}
+
+	public void setLabelBundle(ResourceBundle labelBundle) {
+		this.labelBundle = labelBundle;
+	}
 }
