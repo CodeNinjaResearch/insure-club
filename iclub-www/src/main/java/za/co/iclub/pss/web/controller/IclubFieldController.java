0f6cecec34b37713a94de5be7c5002eb159baf53
sairamdikondawar <sairamdikondawar@gmail.com>
2015-03-27 17:33:49 +0530

    updated field Controller

---
+++ iclub-www/src/main/java/za/co/iclub/pss/web/controller/IclubFieldController.java
@@ -0,0 +1,327 @@
+package za.co.iclub.pss.web.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.log4j.Logger;
+
+import za.co.iclub.pss.web.bean.IclubEntityTypeBean;
+import za.co.iclub.pss.web.bean.IclubFieldBean;
+import za.co.iclub.pss.web.util.IclubWebHelper;
+import za.co.iclub.pss.ws.model.IclubEntityTypeModel;
+import za.co.iclub.pss.ws.model.IclubFieldModel;
+import za.co.iclub.pss.ws.model.common.ResponseModel;
+
+@ManagedBean(name = "iclubFieldController")
+@SessionScoped
+public class IclubFieldController implements Serializable {
+
+	private static final long serialVersionUID = -8789071849340702381L;
+	private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("iclub-web");
+	protected static final Logger LOGGER = Logger.getLogger(IclubFieldController.class);
+	private static final String BASE_URL = "http://" + BUNDLE.getString("ws.host") + ":" + BUNDLE.getString("ws.port") + "/iclub-ws/iclub/IclubFieldService/";
+	private static final String E_BASE_URL = "http://" + BUNDLE.getString("ws.host") + ":" + BUNDLE.getString("ws.port") + "/iclub-ws/iclub/IclubEntityTypeService/";
+	private List<IclubFieldBean> beans;
+	private List<IclubFieldBean> dashBoardBeans;
+	private List<IclubEntityTypeBean> entityTypeBeans;
+	private IclubFieldBean bean;
+	private boolean showCreateCont;
+	private boolean showViewCont;
+	private boolean showEditCont;
+	private boolean showSummaryCont;
+	private Long viewParam;
+	private String sessionUserId;
+	private String userName;
+	private ResourceBundle labelBundle;
+
+	public void initializePage() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: initializePage");
+		if (viewParam == null || viewParam.longValue() == 1)
+			showView();
+		else if (viewParam != null && viewParam.longValue() == 2)
+			showEdit();
+		else if (viewParam != null && viewParam.longValue() == 3)
+			showSummary();
+
+	}
+
+	public void showView() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: showView");
+		showCreateCont = false;
+		showViewCont = true;
+		showEditCont = false;
+		viewParam = 1l;
+	}
+
+	public void showCreate() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: showCreate");
+		bean = new IclubFieldBean();
+		showCreateCont = true;
+		showViewCont = false;
+		showEditCont = false;
+		viewParam = 1l;
+	}
+
+	public void showEdit() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: showEdit");
+		showCreateCont = false;
+		showViewCont = false;
+		showEditCont = true;
+		viewParam = 2l;
+	}
+
+	public void showSummary() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: showSummary");
+		showCreateCont = false;
+		showViewCont = false;
+		showEditCont = false;
+		showSummaryCont = true;
+		viewParam = 3l;
+	}
+
+	public List<IclubFieldBean> getDashBoardBeans() {
+		WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "/get/user/" + getSessionUserId());
+		Collection<? extends IclubFieldModel> models = new ArrayList<IclubFieldModel>(client.accept(MediaType.APPLICATION_JSON).getCollection(IclubFieldModel.class));
+		client.close();
+		dashBoardBeans = new ArrayList<IclubFieldBean>();
+		for (IclubFieldModel model : models) {
+			IclubFieldBean bean = new IclubFieldBean();
+
+			bean.setFId(model.getFId());
+			bean.setFName(model.getFName());
+			bean.setFDesc(model.getFDesc());
+			bean.setFStatus(model.getFStatus());
+			bean.setIclubEntityType(model.getIclubEntityType());
+
+			dashBoardBeans.add(bean);
+		}
+		return dashBoardBeans;
+	}
+
+	public void setDashBoardBeans(List<IclubFieldBean> dashBoardBeans) {
+		this.dashBoardBeans = dashBoardBeans;
+	}
+
+	public void clearForm() {
+		showCreateCont = false;
+		showEditCont = false;
+		bean = new IclubFieldBean();
+	}
+
+	public void addIclubField() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: addIclubField");
+		try {
+			if (validateForm(true)) {
+				WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "add");
+				IclubFieldModel model = new IclubFieldModel();
+
+				model.setFName(bean.getFName());
+				model.setFDesc(bean.getFDesc());
+				model.setFStatus(bean.getFStatus());
+				model.setIclubEntityType(bean.getIclubEntityType());
+				ResponseModel response = client.accept(MediaType.APPLICATION_JSON).post(model, ResponseModel.class);
+				client.close();
+				if (response.getStatusCode() == 0) {
+
+					IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("add.success"), FacesMessage.SEVERITY_INFO);
+					viewParam = 1l;
+					showView();
+				} else {
+					IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("add.error") + " :: " + response.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("add.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+
+	public void modIclubField() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: modIclubField");
+		try {
+			if (validateForm(false)) {
+				WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "mod");
+				IclubFieldModel model = new IclubFieldModel();
+
+				model.setFId(bean.getFId());
+				model.setFName(bean.getFName());
+				model.setFDesc(bean.getFDesc());
+				model.setFStatus(bean.getFStatus());
+				model.setIclubEntityType(bean.getIclubEntityType());
+				ResponseModel response = client.accept(MediaType.APPLICATION_JSON).put(model, ResponseModel.class);
+				client.close();
+				if (response.getStatusCode() == 0) {
+					IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("mod.success"), FacesMessage.SEVERITY_INFO);
+					viewParam = 1l;
+					showView();
+				} else {
+					IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("mod.error") + " :: " + response.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("mod.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+
+	public void delIclubField() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: delIclubField");
+		try {
+			WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "del/" + bean.getFId());
+			Response response = client.accept(MediaType.APPLICATION_JSON).get();
+			if (response.getStatus() == 200) {
+				IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("del.success"), FacesMessage.SEVERITY_INFO);
+				viewParam = 1l;
+				showView();
+			} else {
+				IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("del.service.error"), FacesMessage.SEVERITY_ERROR);
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("geoloc") + " " + getLabelBundle().getString("del.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+
+	public boolean validateForm(boolean flag) {
+		boolean ret = true;
+
+		return ret;
+	}
+
+	public IclubFieldBean getBean() {
+		if (bean == null)
+			bean = new IclubFieldBean();
+		return bean;
+	}
+
+	public void setBean(IclubFieldBean bean) {
+		this.bean = bean;
+	}
+
+	public boolean isShowCreateCont() {
+		return showCreateCont;
+	}
+
+	public void setShowCreateCont(boolean showCreateCont) {
+		this.showCreateCont = showCreateCont;
+	}
+
+	public boolean isShowViewCont() {
+		return showViewCont;
+	}
+
+	public void setShowViewCont(boolean showViewCont) {
+		this.showViewCont = showViewCont;
+	}
+
+	public boolean isShowEditCont() {
+		return showEditCont;
+	}
+
+	public void setShowEditCont(boolean showEditCont) {
+		this.showEditCont = showEditCont;
+	}
+
+	public Long getViewParam() {
+		return viewParam;
+	}
+
+	public void setViewParam(Long viewParam) {
+		this.viewParam = viewParam;
+	}
+
+	public String getSessionUserId() {
+		sessionUserId = IclubWebHelper.getObjectIntoSession(BUNDLE.getString("logged.in.user.id")).toString();
+		return sessionUserId;
+	}
+
+	public void setSessionUserId(String sessionUserId) {
+		this.sessionUserId = sessionUserId;
+	}
+
+	public String getUserName() {
+		userName = IclubWebHelper.getObjectIntoSession(BUNDLE.getString("logged.in.user.scname")).toString();
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public ResourceBundle getLabelBundle() {
+		if (labelBundle == null) {
+			labelBundle = FacesContext.getCurrentInstance().getApplication().getResourceBundle(FacesContext.getCurrentInstance(), "labels");
+		}
+		return labelBundle;
+	}
+
+	public void setLabelBundle(ResourceBundle labelBundle) {
+		this.labelBundle = labelBundle;
+	}
+
+	public List<IclubFieldBean> getBeans() {
+
+		WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "list");
+		Collection<? extends IclubFieldModel> models = new ArrayList<IclubFieldModel>(client.accept(MediaType.APPLICATION_JSON).getCollection(IclubFieldModel.class));
+		client.close();
+		beans = new ArrayList<IclubFieldBean>();
+		for (IclubFieldModel model : models) {
+
+			IclubFieldBean bean = new IclubFieldBean();
+
+			bean.setFId(model.getFId());
+			bean.setFName(model.getFName());
+			bean.setFDesc(model.getFDesc());
+			bean.setFStatus(model.getFStatus());
+			bean.setIclubEntityType(model.getIclubEntityType());
+
+			beans.add(bean);
+		}
+		return beans;
+	}
+
+	public void setBeans(List<IclubFieldBean> beans) {
+		this.beans = beans;
+	}
+
+	public boolean isShowSummaryCont() {
+		return showSummaryCont;
+	}
+
+	public void setShowSummaryCont(boolean showSummaryCont) {
+		this.showSummaryCont = showSummaryCont;
+	}
+
+	public List<IclubEntityTypeBean> getEntityTypeBeans() {
+
+		WebClient client = IclubWebHelper.createCustomClient(E_BASE_URL + "list");
+		Collection<? extends IclubEntityTypeModel> models = new ArrayList<IclubEntityTypeModel>(client.accept(MediaType.APPLICATION_JSON).getCollection(IclubEntityTypeModel.class));
+		client.close();
+		entityTypeBeans = new ArrayList<IclubEntityTypeBean>();
+		for (IclubEntityTypeModel model : models) {
+			IclubEntityTypeBean bean = new IclubEntityTypeBean();
+			bean.setEtId(model.getEtId());
+			bean.setEtLongDesc(model.getEtLongDesc());
+			bean.setEtShortDesc(model.getEtShortDesc());
+			bean.setEtStatus(model.getEtStatus());
+			entityTypeBeans.add(bean);
+		}
+		return entityTypeBeans;
+	}
+
+	public void setEntityTypeBeans(List<IclubEntityTypeBean> entityTypeBeans) {
+		this.entityTypeBeans = entityTypeBeans;
+	}
+
+}
