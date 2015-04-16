72c6a8e85df6d65e93077ebf88c65d7de021579a
vmpattamatta-pss <vmpattamatta@pattamatta.co.in>
2015-04-16 14:53:13 +0200

    update

--- iclub-www/src/main/java/za/co/iclub/pss/web/controller/IclubFullQuoteController.java
+++ iclub-www/src/main/java/za/co/iclub/pss/web/controller/IclubFullQuoteController.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
