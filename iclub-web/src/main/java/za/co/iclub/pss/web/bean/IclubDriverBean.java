7fdf357b63b21d7aee5914e29ba6a605ab8ac276
vpattamatta-pss <vmpattamatta@pattamatta.co.in>
2015-05-08 13:56:23 +0200

    update

--- iclub-web/src/main/java/za/co/iclub/pss/web/bean/IclubDriverBean.java
+++ iclub-web/src/main/java/za/co/iclub/pss/web/bean/IclubDriverBean.java
@@ -3,9 +3,9 @@ package za.co.iclub.pss.web.bean;
 import java.util.Date;
 
 public class IclubDriverBean {
-
+	
 	private String DId;
-	private Long iclubAccessType;
+	private Long iclubAccessTypeByDAccessTypeId;
 	private Long iclubLicenseCode;
 	private String iclubPersonByDCrtdBy;
 	private Long iclubMaritialStatus;
@@ -16,100 +16,100 @@ public class IclubDriverBean {
 	private Date DDob;
 	private Date DCrtdDt;
 	private String[] iclubVehicles;
-
+	
 	public String getDId() {
 		return DId;
 	}
-
+	
 	public void setDId(String dId) {
 		DId = dId;
 	}
-
-	public Long getIclubAccessType() {
-		return iclubAccessType;
-	}
-
-	public void setIclubAccessType(Long iclubAccessType) {
-		this.iclubAccessType = iclubAccessType;
-	}
-
+	
 	public Long getIclubLicenseCode() {
 		return iclubLicenseCode;
 	}
-
+	
 	public void setIclubLicenseCode(Long iclubLicenseCode) {
 		this.iclubLicenseCode = iclubLicenseCode;
 	}
-
+	
 	public String getIclubPersonByDCrtdBy() {
 		return iclubPersonByDCrtdBy;
 	}
-
+	
 	public void setIclubPersonByDCrtdBy(String iclubPersonByDCrtdBy) {
 		this.iclubPersonByDCrtdBy = iclubPersonByDCrtdBy;
 	}
-
+	
 	public Long getIclubMaritialStatus() {
 		return iclubMaritialStatus;
 	}
-
+	
 	public void setIclubMaritialStatus(Long iclubMaritialStatus) {
 		this.iclubMaritialStatus = iclubMaritialStatus;
 	}
-
+	
 	public String getIclubPersonByDPersonId() {
 		return iclubPersonByDPersonId;
 	}
-
+	
 	public void setIclubPersonByDPersonId(String iclubPersonByDPersonId) {
 		this.iclubPersonByDPersonId = iclubPersonByDPersonId;
 	}
-
+	
 	public String getDName() {
 		return DName;
 	}
-
+	
 	public void setDName(String dName) {
 		DName = dName;
 	}
-
+	
 	public String getDLicenseNum() {
 		return DLicenseNum;
 	}
-
+	
 	public void setDLicenseNum(String dLicenseNum) {
 		DLicenseNum = dLicenseNum;
 	}
-
+	
 	public Date getDIssueDt() {
 		return DIssueDt;
 	}
-
+	
 	public void setDIssueDt(Date dIssueDt) {
 		DIssueDt = dIssueDt;
 	}
-
+	
 	public Date getDDob() {
 		return DDob;
 	}
-
+	
 	public void setDDob(Date dDob) {
 		DDob = dDob;
 	}
-
+	
 	public Date getDCrtdDt() {
 		return DCrtdDt;
 	}
-
+	
 	public void setDCrtdDt(Date dCrtdDt) {
 		DCrtdDt = dCrtdDt;
 	}
-
+	
 	public String[] getIclubVehicles() {
 		return iclubVehicles;
 	}
-
+	
 	public void setIclubVehicles(String[] iclubVehicles) {
 		this.iclubVehicles = iclubVehicles;
+	}
+	
+	public Long getIclubAccessTypeByDAccessTypeId() {
+		return iclubAccessTypeByDAccessTypeId;
+	}
+	
+	public void setIclubAccessTypeByDAccessTypeId(Long iclubAccessTypeByDAccessTypeId) {
+		this.iclubAccessTypeByDAccessTypeId = iclubAccessTypeByDAccessTypeId;
 	}
 }
