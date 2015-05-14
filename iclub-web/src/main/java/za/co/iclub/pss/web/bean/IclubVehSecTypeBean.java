4d1ac5ed9f207746cfe18f64fd6c92b137adbe95
vpattamatta-pss <vmpattamatta@pattamatta.co.in>
2015-05-14 14:12:14 +0200

    update

---
+++ iclub-web/src/main/java/za/co/iclub/pss/web/bean/IclubVehSecTypeBean.java
@@ -0,0 +1,51 @@
+package za.co.iclub.pss.web.bean;
+
+
+public class IclubVehSecTypeBean {
+	
+	private Long vstId;
+	private String vstShortDesc;
+	private String vstLongDesc;
+	private String vstStatus;
+	private String[] iclubVehicles;
+	
+	public Long getVstId() {
+		return vstId;
+	}
+	
+	public void setVstId(Long vstId) {
+		this.vstId = vstId;
+	}
+	
+	public String getVstShortDesc() {
+		return vstShortDesc;
+	}
+	
+	public void setVstShortDesc(String vstShortDesc) {
+		this.vstShortDesc = vstShortDesc;
+	}
+	
+	public String getVstLongDesc() {
+		return vstLongDesc;
+	}
+	
+	public void setVstLongDesc(String vstLongDesc) {
+		this.vstLongDesc = vstLongDesc;
+	}
+	
+	public String getVstStatus() {
+		return vstStatus;
+	}
+	
+	public void setVstStatus(String vstStatus) {
+		this.vstStatus = vstStatus;
+	}
+	
+	public String[] getIclubVehicles() {
+		return iclubVehicles;
+	}
+	
+	public void setIclubVehicles(String[] iclubVehicles) {
+		this.iclubVehicles = iclubVehicles;
+	}
+}
---
+++ iclub-web/src/main/java/za/co/iclub/pss/web/controller/IclubPropSecTypeController.java
@@ -0,0 +1,215 @@
+package za.co.iclub.pss.web.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.log4j.Logger;
+
+import za.co.iclub.pss.web.bean.IclubPropSecTypeBean;
+import za.co.iclub.pss.web.util.IclubWebHelper;
+import za.co.iclub.pss.ws.model.IclubPropSecTypeModel;
+import za.co.iclub.pss.ws.model.common.ResponseModel;
+
+@ManagedBean(name = "iclubPropSecTypeController")
+@SessionScoped
+public class IclubPropSecTypeController implements Serializable {
+	
+	private static final long serialVersionUID = 6271776777151313314L;
+	private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("iclub-web");
+	private static final Logger LOGGER = Logger.getLogger(IclubPropSecTypeController.class);
+	private static final String BASE_URL = "http://" + BUNDLE.getString("ws.host") + ":" + BUNDLE.getString("ws.port") + "/iclub-ws/iclub/IclubPropSecTypeService/";
+	private List<IclubPropSecTypeBean> beans;
+	private IclubPropSecTypeBean bean;
+	private boolean showAddPanel;
+	private boolean showModPanel;
+	private ResourceBundle labelBundle;
+	
+	public void addIclubPropSecType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: addIclubPropSecType");
+		try {
+			if (validateForm(true)) {
+				WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "add");
+				IclubPropSecTypeModel model = new IclubPropSecTypeModel();
+				
+				model.setPstLongDesc(bean.getPstLongDesc());
+				model.setPstShortDesc(bean.getPstShortDesc());
+				model.setPstStatus(bean.getPstStatus());
+				
+				ResponseModel response = client.accept(MediaType.APPLICATION_JSON).post(model, ResponseModel.class);
+				client.close();
+				if (response.getStatusCode() == 0) {
+					IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("add.success"), FacesMessage.SEVERITY_INFO);
+					clearForm();
+				} else {
+					IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("add.error") + " :: " + response.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("add.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+	
+	public void modIclubPropSecType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: modIclubPropSecType");
+		try {
+			if (validateForm(false)) {
+				WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "mod");
+				IclubPropSecTypeModel model = new IclubPropSecTypeModel();
+				model.setPstId(bean.getPstId());
+				model.setPstLongDesc(bean.getPstLongDesc());
+				model.setPstShortDesc(bean.getPstShortDesc());
+				model.setPstStatus(bean.getPstStatus());
+				
+				ResponseModel response = client.accept(MediaType.APPLICATION_JSON).put(model, ResponseModel.class);
+				client.close();
+				if (response.getStatusCode() == 0) {
+					IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("mod.success"), FacesMessage.SEVERITY_INFO);
+					clearForm();
+				} else {
+					IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("mod.error") + " :: " + response.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("mod.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+	
+	public void delIclubPropSecType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: delIclubPropSecType");
+		try {
+			WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "del/" + bean.getPstId());
+			Response response = client.accept(MediaType.APPLICATION_JSON).get();
+			if (response.getStatus() == 200) {
+				IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("del.success"), FacesMessage.SEVERITY_INFO);
+				clearForm();
+			} else {
+				IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("del.service.error"), FacesMessage.SEVERITY_ERROR);
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("propusage") + " " + getLabelBundle().getString("del.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+	
+	public void clearForm() {
+		showAddPanel = false;
+		showModPanel = false;
+		bean = new IclubPropSecTypeBean();
+	}
+	
+	public void showAddPanel() {
+		showAddPanel = true;
+		showModPanel = false;
+		bean = new IclubPropSecTypeBean();
+	}
+	
+	public void showModPanel() {
+		showAddPanel = false;
+		showModPanel = true;
+	}
+	
+	public boolean validateForm(boolean flag) {
+		boolean ret = true;
+		
+		if (bean.getPstShortDesc() != null && !bean.getPstShortDesc().trim().equalsIgnoreCase("")) {
+			WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "validate/sd/" + bean.getPstShortDesc().trim() + "/" + ((bean.getPstId() == null) ? -999l : bean.getPstId()));
+			ResponseModel message = client.accept(MediaType.APPLICATION_JSON).get(ResponseModel.class);
+			client.close();
+			if (message.getStatusCode() != 0) {
+				IclubWebHelper.addMessage(message.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				ret = ret && false;
+			}
+		} else {
+			IclubWebHelper.addMessage(getLabelBundle().getString("val.shortdesc.empty"), FacesMessage.SEVERITY_ERROR);
+			ret = ret && false;
+		}
+		
+		if (bean.getPstLongDesc() == null || bean.getPstLongDesc().trim().equalsIgnoreCase("")) {
+			IclubWebHelper.addMessage(getLabelBundle().getString("val.longdesc.empty"), FacesMessage.SEVERITY_ERROR);
+			ret = ret && false;
+		}
+		
+		if (bean.getPstStatus() == null || bean.getPstStatus().trim().equalsIgnoreCase("")) {
+			IclubWebHelper.addMessage(getLabelBundle().getString("val.select.valid"), FacesMessage.SEVERITY_ERROR);
+			ret = ret && false;
+		}
+		
+		return ret;
+	}
+	
+	public List<IclubPropSecTypeBean> getBeans() {
+		WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "list");
+		Collection<? extends IclubPropSecTypeModel> models = new ArrayList<IclubPropSecTypeModel>(client.accept(MediaType.APPLICATION_JSON).getCollection(IclubPropSecTypeModel.class));
+		client.close();
+		beans = new ArrayList<IclubPropSecTypeBean>();
+		if (models != null && models.size() > 0) {
+			for (IclubPropSecTypeModel model : models) {
+				IclubPropSecTypeBean bean = new IclubPropSecTypeBean();
+				bean.setPstId(model.getPstId());
+				bean.setPstLongDesc(model.getPstLongDesc());
+				bean.setPstShortDesc(model.getPstShortDesc());
+				bean.setPstStatus(model.getPstStatus());
+				if (model.getIclubProperties() != null && model.getIclubProperties().length > 0) {
+					String[] properties = model.getIclubProperties();
+					bean.setIclubProperties(properties);
+					
+				}
+				beans.add(bean);
+			}
+		}
+		return beans;
+	}
+	
+	public void setBeans(List<IclubPropSecTypeBean> beans) {
+		this.beans = beans;
+	}
+	
+	public IclubPropSecTypeBean getBean() {
+		if (bean == null)
+			bean = new IclubPropSecTypeBean();
+		return bean;
+	}
+	
+	public void setBean(IclubPropSecTypeBean bean) {
+		this.bean = bean;
+	}
+	
+	public boolean isShowAddPanel() {
+		return showAddPanel;
+	}
+	
+	public void setShowAddPanel(boolean showAddPanel) {
+		this.showAddPanel = showAddPanel;
+	}
+	
+	public boolean isShowModPanel() {
+		return showModPanel;
+	}
+	
+	public void setShowModPanel(boolean showModPanel) {
+		this.showModPanel = showModPanel;
+	}
+	
+	public ResourceBundle getLabelBundle() {
+		
+		labelBundle = FacesContext.getCurrentInstance().getApplication().getResourceBundle(FacesContext.getCurrentInstance(), "labels");
+		return labelBundle;
+	}
+	
+	public void setLabelBundle(ResourceBundle labelBundle) {
+		this.labelBundle = labelBundle;
+	}
+}
