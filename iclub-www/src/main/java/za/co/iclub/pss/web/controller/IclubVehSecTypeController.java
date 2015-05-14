949493213707250f9779e541dd78700ae44e29a5
sairamdikondawar <sairamdikondawar@gmail.com>
2015-05-14 16:51:24 +0530

    update

---
+++ iclub-www/src/main/java/za/co/iclub/pss/web/controller/IclubVehSecTypeController.java
@@ -0,0 +1,214 @@
+package za.co.iclub.pss.web.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.log4j.Logger;
+
+import za.co.iclub.pss.web.bean.IclubVehSecTypeBean;
+import za.co.iclub.pss.web.util.IclubWebHelper;
+import za.co.iclub.pss.ws.model.IclubVehSecTypeModel;
+import za.co.iclub.pss.ws.model.common.ResponseModel;
+
+@ManagedBean(name = "iclubVehSecTypeController")
+@SessionScoped
+public class IclubVehSecTypeController implements Serializable {
+	
+	private static final long serialVersionUID = 6271776777151313314L;
+	private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("iclub-web");
+	private static final Logger LOGGER = Logger.getLogger(IclubVehSecTypeController.class);
+	private static final String BASE_URL = "http://" + BUNDLE.getString("ws.host") + ":" + BUNDLE.getString("ws.port") + "/iclub-ws/iclub/IclubVehSecTypeService/";
+	private List<IclubVehSecTypeBean> beans;
+	private IclubVehSecTypeBean bean;
+	private boolean showAddPanel;
+	private boolean showModPanel;
+	private ResourceBundle labelBundle;
+	
+	public void addIclubVehSecType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: addIclubVehSecType");
+		try {
+			if (validateForm(true)) {
+				WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "add");
+				IclubVehSecTypeModel model = new IclubVehSecTypeModel();
+				
+				model.setVstLongDesc(bean.getVstLongDesc());
+				model.setVstShortDesc(bean.getVstShortDesc());
+				model.setVstStatus(bean.getVstStatus());
+				
+				ResponseModel response = client.accept(MediaType.APPLICATION_JSON).post(model, ResponseModel.class);
+				client.close();
+				if (response.getStatusCode() == 0) {
+					IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("add.success"), FacesMessage.SEVERITY_INFO);
+					clearForm();
+				} else {
+					IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("add.error") + " :: " + response.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("add.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+	
+	public void modIclubVehSecType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: modIclubVehSecType");
+		try {
+			if (validateForm(false)) {
+				WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "mod");
+				IclubVehSecTypeModel model = new IclubVehSecTypeModel();
+				model.setVstId(bean.getVstId());
+				model.setVstLongDesc(bean.getVstLongDesc());
+				model.setVstShortDesc(bean.getVstShortDesc());
+				model.setVstStatus(bean.getVstStatus());
+				
+				ResponseModel response = client.accept(MediaType.APPLICATION_JSON).put(model, ResponseModel.class);
+				client.close();
+				if (response.getStatusCode() == 0) {
+					IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("mod.success"), FacesMessage.SEVERITY_INFO);
+					clearForm();
+				} else {
+					IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("mod.error") + " :: " + response.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("mod.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+	
+	public void delIclubVehSecType() {
+		LOGGER.info("Class :: " + this.getClass() + " :: Method :: delIclubVehSecType");
+		try {
+			WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "del/" + bean.getVstId());
+			Response response = client.accept(MediaType.APPLICATION_JSON).get();
+			if (response.getStatus() == 200) {
+				IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("del.success"), FacesMessage.SEVERITY_INFO);
+				clearForm();
+			} else {
+				IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("del.service.error"), FacesMessage.SEVERITY_ERROR);
+			}
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			IclubWebHelper.addMessage(getLabelBundle().getString("vehusage") + " " + getLabelBundle().getString("del.error") + " :: " + e.getMessage(), FacesMessage.SEVERITY_ERROR);
+		}
+	}
+	
+	public void clearForm() {
+		showAddPanel = false;
+		showModPanel = false;
+		bean = new IclubVehSecTypeBean();
+	}
+	
+	public void showAddPanel() {
+		showAddPanel = true;
+		showModPanel = false;
+		bean = new IclubVehSecTypeBean();
+	}
+	
+	public void showModPanel() {
+		showAddPanel = false;
+		showModPanel = true;
+	}
+	
+	public boolean validateForm(boolean flag) {
+		boolean ret = true;
+		
+		if (bean.getVstShortDesc() != null && !bean.getVstShortDesc().trim().equalsIgnoreCase("")) {
+			WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "validate/sd/" + bean.getVstShortDesc().trim() + "/" + ((bean.getVstId() == null) ? -999l : bean.getVstId()));
+			ResponseModel message = client.accept(MediaType.APPLICATION_JSON).get(ResponseModel.class);
+			client.close();
+			if (message.getStatusCode() != 0) {
+				IclubWebHelper.addMessage(message.getStatusDesc(), FacesMessage.SEVERITY_ERROR);
+				ret = ret && false;
+			}
+		} else {
+			IclubWebHelper.addMessage(getLabelBundle().getString("val.shortdesc.empty"), FacesMessage.SEVERITY_ERROR);
+			ret = ret && false;
+		}
+		
+		if (bean.getVstLongDesc() == null || bean.getVstLongDesc().trim().equalsIgnoreCase("")) {
+			IclubWebHelper.addMessage(getLabelBundle().getString("val.longdesc.empty"), FacesMessage.SEVERITY_ERROR);
+			ret = ret && false;
+		}
+		
+		if (bean.getVstStatus() == null || bean.getVstStatus().trim().equalsIgnoreCase("")) {
+			IclubWebHelper.addMessage(getLabelBundle().getString("val.select.valid"), FacesMessage.SEVERITY_ERROR);
+			ret = ret && false;
+		}
+		
+		return ret;
+	}
+	
+	public List<IclubVehSecTypeBean> getBeans() {
+		WebClient client = IclubWebHelper.createCustomClient(BASE_URL + "list");
+		Collection<? extends IclubVehSecTypeModel> models = new ArrayList<IclubVehSecTypeModel>(client.accept(MediaType.APPLICATION_JSON).getCollection(IclubVehSecTypeModel.class));
+		client.close();
+		beans = new ArrayList<IclubVehSecTypeBean>();
+		if (models != null && models.size() > 0) {
+			for (IclubVehSecTypeModel model : models) {
+				IclubVehSecTypeBean bean = new IclubVehSecTypeBean();
+				bean.setVstId(model.getVstId());
+				bean.setVstLongDesc(model.getVstLongDesc());
+				bean.setVstShortDesc(model.getVstShortDesc());
+				bean.setVstStatus(model.getVstStatus());
+				if (model.getIclubVehicles() != null && model.getIclubVehicles().length > 0) {
+					String[] vehicles = model.getIclubVehicles();
+					bean.setIclubVehicles(vehicles);
+				}
+				beans.add(bean);
+			}
+		}
+		return beans;
+	}
+	
+	public void setBeans(List<IclubVehSecTypeBean> beans) {
+		this.beans = beans;
+	}
+	
+	public IclubVehSecTypeBean getBean() {
+		if (bean == null)
+			bean = new IclubVehSecTypeBean();
+		return bean;
+	}
+	
+	public void setBean(IclubVehSecTypeBean bean) {
+		this.bean = bean;
+	}
+	
+	public boolean isShowAddPanel() {
+		return showAddPanel;
+	}
+	
+	public void setShowAddPanel(boolean showAddPanel) {
+		this.showAddPanel = showAddPanel;
+	}
+	
+	public boolean isShowModPanel() {
+		return showModPanel;
+	}
+	
+	public void setShowModPanel(boolean showModPanel) {
+		this.showModPanel = showModPanel;
+	}
+	
+	public ResourceBundle getLabelBundle() {
+		
+		labelBundle = FacesContext.getCurrentInstance().getApplication().getResourceBundle(FacesContext.getCurrentInstance(), "labels");
+		return labelBundle;
+	}
+	
+	public void setLabelBundle(ResourceBundle labelBundle) {
+		this.labelBundle = labelBundle;
+	}
+}
