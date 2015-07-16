e3b7705ea5d8ec645f673a84566270854ad36e81
sairamdikondawar <sairamdikondawar@gmail.com>
2015-07-16 12:28:20 +0530

    update userdashboard

---
+++ iclub-app/src/main/java/za/co/iclub/pss/model/ws/IclubUserDashboardModel.java
@@ -0,0 +1,157 @@
+package za.co.iclub.pss.model.ws;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "IclubUserDashboardModel")
+public class IclubUserDashboardModel {
+	
+	private Long totalQuoteCnt;
+	private Double totalQPremium;
+	private Double totalQEstValue;
+	private Date earliestQExpiry;
+	
+	private Long totalPaymentsCnt;
+	private Double totalPyPremium;
+	private Double totalCPremium;
+	private Date nextPremiumDate;
+	
+	private Long totalPolicyCnt;
+	private Double totalPPremium;
+	private Long noOfVehicles;
+	private Long noOfProperties;
+	
+	private Long totalClaimCnt;
+	private Double totalClaimValue;
+	private Double rejectedClaimValue;
+	private Double approvedClaimValue;
+	
+	public Long getTotalQuoteCnt() {
+		return totalQuoteCnt;
+	}
+	
+	public void setTotalQuoteCnt(Long totalQuoteCnt) {
+		this.totalQuoteCnt = totalQuoteCnt;
+	}
+	
+	public Double getTotalQPremium() {
+		return totalQPremium;
+	}
+	
+	public void setTotalQPremium(Double totalQPremium) {
+		this.totalQPremium = totalQPremium;
+	}
+	
+	public Double getTotalQEstValue() {
+		return totalQEstValue;
+	}
+	
+	public void setTotalQEstValue(Double totalQEstValue) {
+		this.totalQEstValue = totalQEstValue;
+	}
+	
+	public Date getEarliestQExpiry() {
+		return earliestQExpiry;
+	}
+	
+	public void setEarliestQExpiry(Date earliestQExpiry) {
+		this.earliestQExpiry = earliestQExpiry;
+	}
+	
+	public Long getTotalPaymentsCnt() {
+		return totalPaymentsCnt;
+	}
+	
+	public void setTotalPaymentsCnt(Long totalPaymentsCnt) {
+		this.totalPaymentsCnt = totalPaymentsCnt;
+	}
+	
+	public Double getTotalPyPremium() {
+		return totalPyPremium;
+	}
+	
+	public void setTotalPyPremium(Double totalPyPremium) {
+		this.totalPyPremium = totalPyPremium;
+	}
+	
+	public Double getTotalCPremium() {
+		return totalCPremium;
+	}
+	
+	public void setTotalCPremium(Double totalCPremium) {
+		this.totalCPremium = totalCPremium;
+	}
+	
+	public Date getNextPremiumDate() {
+		return nextPremiumDate;
+	}
+	
+	public void setNextPremiumDate(Date nextPremiumDate) {
+		this.nextPremiumDate = nextPremiumDate;
+	}
+	
+	public Long getTotalPolicyCnt() {
+		return totalPolicyCnt;
+	}
+	
+	public void setTotalPolicyCnt(Long totalPolicyCnt) {
+		this.totalPolicyCnt = totalPolicyCnt;
+	}
+	
+	public Double getTotalPPremium() {
+		return totalPPremium;
+	}
+	
+	public void setTotalPPremium(Double totalPPremium) {
+		this.totalPPremium = totalPPremium;
+	}
+	
+	public Long getNoOfVehicles() {
+		return noOfVehicles;
+	}
+	
+	public void setNoOfVehicles(Long noOfVehicles) {
+		this.noOfVehicles = noOfVehicles;
+	}
+	
+	public Long getNoOfProperties() {
+		return noOfProperties;
+	}
+	
+	public void setNoOfProperties(Long noOfProperties) {
+		this.noOfProperties = noOfProperties;
+	}
+	
+	public Long getTotalClaimCnt() {
+		return totalClaimCnt;
+	}
+	
+	public void setTotalClaimCnt(Long totalClaimCnt) {
+		this.totalClaimCnt = totalClaimCnt;
+	}
+	
+	public Double getTotalClaimValue() {
+		return totalClaimValue;
+	}
+	
+	public void setTotalClaimValue(Double totalClaimValue) {
+		this.totalClaimValue = totalClaimValue;
+	}
+	
+	public Double getRejectedClaimValue() {
+		return rejectedClaimValue;
+	}
+	
+	public void setRejectedClaimValue(Double rejectedClaimValue) {
+		this.rejectedClaimValue = rejectedClaimValue;
+	}
+	
+	public Double getApprovedClaimValue() {
+		return approvedClaimValue;
+	}
+	
+	public void setApprovedClaimValue(Double approvedClaimValue) {
+		this.approvedClaimValue = approvedClaimValue;
+	}
+}
---
+++ iclub-app/src/main/java/za/co/iclub/pss/ws/service/IclubUserDashboardService.java
@@ -0,0 +1,206 @@
+package za.co.iclub.pss.ws.service;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.apache.log4j.Logger;
+import org.springframework.transaction.annotation.Transactional;
+
+import za.co.iclub.pss.model.ws.IclubUserDashboardModel;
+import za.co.iclub.pss.orm.dao.IclubNamedQueryDAO;
+
+@Path(value = "/IclubUserDashboardService")
+@SuppressWarnings({ "rawtypes" })
+public class IclubUserDashboardService {
+	
+	private static final Logger LOGGER = Logger.getLogger(IclubUserDashboardService.class);
+	private IclubNamedQueryDAO iclubNamedQueryDAO;
+	
+	@GET
+	@Path("/get/{userId}")
+	@Consumes("application/json")
+	@Produces("application/json")
+	@Transactional
+	public IclubUserDashboardModel validateSd(@PathParam("userId") String userId) {
+		try {
+			IclubUserDashboardModel model = new IclubUserDashboardModel();
+			String quoteIds = "";
+			Double insuredValue = 0.0;
+			String policyIds = "";
+			String cliamIds = "";
+			
+			try {
+				List data = iclubNamedQueryDAO.getQuoteDetailsByUserId(userId, 1l);
+				
+				if (data != null && data.size() > 0) {
+					Object[] objects = (Object[]) data.get(0);
+					model.setTotalQuoteCnt(objects[0] != null ? new Long(objects[0].toString()) : 0);
+					model.setTotalQPremium(objects[1] != null ? new Double(objects[0].toString()) : 0);
+				}
+			} catch (Exception e) {
+				LOGGER.error(e, e);
+			}
+			try {
+				List data = iclubNamedQueryDAO.getQuoteDetailsIdByUserId(userId, 1l);
+				
+				if (data != null && data.size() > 0) {
+					
+					for (int i = 0; i < data.size(); i++) {
+						quoteIds = quoteIds.trim().equalsIgnoreCase("") ? "'" + data.get(i).toString() + "'" : quoteIds + ",'" + data.get(i).toString() + "'";
+					}
+					
+					data = iclubNamedQueryDAO.getIclubQuoteIdByUserId(quoteIds);
+					
+					String vehicleIds = "";
+					String propertyIds = "";
+					
+					int noOfVehicles = 0;
+					int noOfProperties = 0;
+					
+					if (data != null && data.size() > 0) {
+						for (int i = 0; i < data.size(); i++) {
+							Object[] object = (Object[]) data.get(i);
+							
+							if (object[0] != null && object[0].toString().equalsIgnoreCase("1")) {
+								vehicleIds = vehicleIds.trim().equalsIgnoreCase("") ? "'" + object[1].toString() + "'" : vehicleIds + ",'" + object[1].toString() + "'";
+								noOfVehicles++;
+							} else if (object[0] != null && object[0].toString().equalsIgnoreCase("2")) {
+								propertyIds = propertyIds.trim().equalsIgnoreCase("") ? "'" + object[1].toString() + "'" : propertyIds + ",'" + object[1].toString() + "'";
+								noOfProperties++;
+							}
+							
+						}
+						model.setNoOfProperties(new Long(noOfProperties));
+						model.setNoOfVehicles(new Long(noOfVehicles));
+						
+						data = iclubNamedQueryDAO.getIclubPropertyIValueByPIds(propertyIds);
+						if (data != null && data.size() > 0) {
+							Object[] object = (Object[]) data.get(0);
+							insuredValue = object[0] != null ? insuredValue + ((Double) object[0]) : insuredValue;
+							insuredValue = object[1] != null ? insuredValue + ((Double) object[1]) : insuredValue;
+						}
+						
+						data = iclubNamedQueryDAO.getIclubVehicleIValueByVIds(vehicleIds);
+						if (data != null && data.size() > 0) {
+							Object[] object = (Object[]) data.get(0);
+							insuredValue = object[0] != null ? insuredValue + ((Double) object[0]) : insuredValue;
+						}
+						model.setTotalQEstValue(insuredValue);
+					}
+				}
+			} catch (Exception e) {
+				LOGGER.error(e, e);
+			}
+			
+			try {
+				Object object = iclubNamedQueryDAO.getQuoteDetailsValDateByUserId(userId, 1l);
+				
+				if (object != null) {
+					model.setEarliestQExpiry((Date) object);
+				}
+			} catch (Exception e) {
+				LOGGER.error(e, e);
+			}
+			
+			try {
+				Object object = iclubNamedQueryDAO.getQuoteDetailsValDateByUserId(userId, 1l);
+				
+				if (object != null) {
+					model.setEarliestQExpiry((Date) object);
+				}
+			} catch (Exception e) {
+				LOGGER.error(e, e);
+			}
+			try {
+				List data = iclubNamedQueryDAO.getIclubPolicIdsByQuotes(quoteIds);
+				
+				if (data != null && data.size() > 0) {
+					
+					Double prorataPremium = insuredValue;
+					for (int i = 0; i < data.size(); i++) {
+						Object[] object = (Object[]) data.get(i);
+						policyIds = policyIds.trim().equalsIgnoreCase("") ? "'" + object[0].toString().toString() + "'" : policyIds + ",'" + object[0].toString().toString() + "'";
+						prorataPremium = object != null && object[1] != null ? new Double(object[1].toString()) + prorataPremium : prorataPremium;
+					}
+					
+					model.setTotalPolicyCnt(new Long(data.size()));
+					model.setTotalPPremium(prorataPremium);
+				}
+			} catch (Exception e) {
+				LOGGER.error(e, e);
+			}
+			
+			try {
+				List data = iclubNamedQueryDAO.getIclubClaimValueByPolicyIds(policyIds);
+				
+				if (data != null && data.size() > 0) {
+					
+					Double claimValue = 0.0;
+					Double rejectedClaimValue = 0.0;
+					Double approvedClaimValue = 0.0;
+					for (int i = 0; i < data.size(); i++) {
+						Object[] object = (Object[]) data.get(i);
+						cliamIds = cliamIds.trim().equalsIgnoreCase("") ? "'" + object[0].toString().toString() + "'" : cliamIds + ",'" + object[0].toString().toString() + "'";
+						claimValue = object != null && object[1] != null ? new Double(object[1].toString()) + claimValue : claimValue;
+						
+						if (object[2] != null && object[2].toString().equalsIgnoreCase("5")) {
+							approvedClaimValue = object != null && object[1] != null ? new Double(object[1].toString()) + approvedClaimValue : approvedClaimValue;
+						}
+						if (object[2] != null && object[2].toString().equalsIgnoreCase("9")) {
+							rejectedClaimValue = object != null && object[1] != null ? new Double(object[1].toString()) + rejectedClaimValue : rejectedClaimValue;
+						}
+					}
+					
+					model.setApprovedClaimValue(approvedClaimValue);
+					model.setRejectedClaimValue(rejectedClaimValue);
+					model.setTotalClaimValue(claimValue);
+				}
+				
+			} catch (Exception e) {
+				LOGGER.error(e, e);
+			}
+			try {
+				int policycount = 0;
+				List data = iclubNamedQueryDAO.getIclubPaymentsByClaimIds(cliamIds);
+				
+				if (data != null && data.size() > 0) {
+					Object[] object = (Object[]) data.get(0);
+					policycount = object != null && object[0] != null ? new Integer(object[0].toString()) : 0;
+					model.setTotalCPremium(object != null && object[1] != null ? new Double(object[1].toString()) : 0.0);
+				}
+				data = iclubNamedQueryDAO.getIclubPaymentByPolicyIds(policyIds);
+				
+				if (data != null && data.size() > 0) {
+					Object[] object = (Object[]) data.get(0);
+					policycount = object != null && object[0] != null ? new Integer(object[0].toString()) + policycount : policycount;
+					model.setTotalPyPremium(object != null && object[1] != null ? new Double(object[1].toString()) : 0.0);
+				}
+				
+				model.setTotalPaymentsCnt(new Long(policycount));
+				
+			} catch (Exception e) {
+				LOGGER.error(e, e);
+			}
+			return model;
+		} catch (Exception e) {
+			LOGGER.error(e, e);
+			throw e;
+		}
+		
+	}
+	
+	public IclubNamedQueryDAO getIclubNamedQueryDAO() {
+		return iclubNamedQueryDAO;
+	}
+	
+	public void setIclubNamedQueryDAO(IclubNamedQueryDAO iclubNamedQueryDAO) {
+		this.iclubNamedQueryDAO = iclubNamedQueryDAO;
+	}
+	
+}
